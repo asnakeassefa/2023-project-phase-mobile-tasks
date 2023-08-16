bb3f4434d9b7c51daddbdc19de4d8e5a1c598b8f
Asnake Assefa <asnakeassefa28@gmail.com>
2023-08-16 09:55:15 +0300

    view all tasks test

---
+++ on-boarding/todo_app_with_clean_arch/test/features/todo/domain/usecases/view_all_tasks_test.dart
@@ -0,0 +1,45 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:mockito/annotations.dart';
+import 'package:todo_app_with_clean_arch/features/todo/domain/entities/task.dart';
+import 'package:todo_app_with_clean_arch/features/todo/domain/repositories/task_repository.dart';
+import 'package:todo_app_with_clean_arch/features/todo/domain/usecases/view_all_tasks.dart';
+
+import 'creat_task_test.mocks.dart';
+
+@GenerateMocks([TaskRepository])
+
+void main(){
+
+  late MockTaskRepository mockTaskRepository;
+  late ViewAllTasks usecase;
+
+
+  setUp((){
+    mockTaskRepository = MockTaskRepository();
+    usecase = ViewAllTasks(mockTaskRepository);
+  });
+
+ final List<TaskEntity> task =[TaskEntity(
+      taskId: 1,
+      taskTitle: 'Test',
+      taskDescription: 'test descri',
+      taskDueDate: DateTime.now().toString(),
+      taskStatus: false)];
+
+
+  test("it displays all tasks", () async{
+
+    when(mockTaskRepository.viewAllTask()).thenAnswer((_) async => Right(task));
+
+    final result = await usecase();
+
+    expect(result, Right(task));
+
+    verify(mockTaskRepository.viewAllTask());
+
+    verifyNoMoreInteractions(mockTaskRepository);
+  });
+
+}
